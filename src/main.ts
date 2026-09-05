f8ea1773365cfa19e859e9662ef6a421fc9df85b
John Helmuth <john@helmuth.org>
2026-09-05 09:42:28 -0700

    Initial code after stripping project template, adding header.

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import './style.css'
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+<h1>Spacewar! - a Vanilla Javascript implementation.</h1>
+<section id="center">
+</section>
+
+
+</section>
+
+`
